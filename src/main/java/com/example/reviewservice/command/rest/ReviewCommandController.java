e11b4b4b49d4810f86f0644b285b55a876dc870d
GamePoro <gamesarawur1@gmail.com>
2022-12-21 14:52:02 +0700

    Add Rating

--- src/main/java/com/example/reviewservice/command/rest/ReviewCommandController.java
+++ src/main/java/com/example/reviewservice/command/rest/ReviewCommandController.java
@@ -3,8 +3,10 @@ package com.example.reviewservice.command.rest;
 
 import com.example.reviewservice.command.CreateReviewCommand;
 import com.example.reviewservice.command.UpdateReviewCommand;
+import com.example.reviewservice.core.ReviewEntity;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,8 +16,10 @@ import java.util.UUID;
 
 @Service
 public class ReviewCommandController {
-
     private final CommandGateway commandGateway;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @Autowired
     public ReviewCommandController(CommandGateway commandGateway){
@@ -35,6 +39,7 @@ public class ReviewCommandController {
                 .timeOpen(model.getTimeOpen())
                 .timeClose(model.getTimeClose())
                 .personReview(model.getPersonReview())
+                .rating(model.getRating())
                 .phone(model.getPhone())
                 .ban(model.isBan())
                 .range(model.getRange())
@@ -84,4 +89,70 @@ public class ReviewCommandController {
 
     }
 
+    @RabbitListener(queues = "UpdateRatingQueue")
+    public void updateRating(String messange){
+//        System.out.println("SS");
+        String[] data = messange.split("__");
+//        System.out.println(data[0] + ":" +data[1]);
+        Object query = rabbitTemplate.convertSendAndReceive("ReviewExchange", "reviewbyid", data[0]);
+        ReviewEntity model = (ReviewEntity) query;
+        UpdateReviewCommand command = UpdateReviewCommand.builder()
+                ._id(model.get_id())
+                .name(model.getName())
+                .branch(model.getBranch())
+                .store_type(model.getStore_type())
+                .description(model.getDescription())
+                .imageId(model.getImageId())
+                .address(model.getAddress())
+                .timeOpen(model.getTimeOpen())
+                .timeClose(model.getTimeClose())
+                .personReview(model.getPersonReview() + 1)
+                .phone(model.getPhone())
+                .rating(model.getRating() + Float.parseFloat(data[1]))
+                .ban(model.isBan())
+                .range(model.getRange())
+                .delivery(model.isDelivery())
+                .pickUp(model.isPickUp())
+                .userId(model.getUserId())
+                .build();
+
+        String result;
+        try {
+            result = commandGateway.sendAndWait(command);
+        }catch (Exception e){
+            result = e.getLocalizedMessage();
+        }
+    }
+    @RabbitListener(queues = "UpdateRatingWithOldCommentQueue")
+    public void updateWithOldCommentRating(String messange){
+        String[] data = messange.split("__");
+        Object query = rabbitTemplate.convertSendAndReceive("ReviewExchange", "reviewbyid", data[0]);
+        ReviewEntity model = (ReviewEntity) query;
+        UpdateReviewCommand command = UpdateReviewCommand.builder()
+                ._id(model.get_id())
+                .name(model.getName())
+                .branch(model.getBranch())
+                .store_type(model.getStore_type())
+                .description(model.getDescription())
+                .imageId(model.getImageId())
+                .address(model.getAddress())
+                .timeOpen(model.getTimeOpen())
+                .timeClose(model.getTimeClose())
+                .personReview(model.getPersonReview())
+                .phone(model.getPhone())
+                .rating(model.getRating() + (Float.parseFloat(data[1])) - 5)
+                .ban(model.isBan())
+                .range(model.getRange())
+                .delivery(model.isDelivery())
+                .pickUp(model.isPickUp())
+                .userId(model.getUserId())
+                .build();
+
+        String result;
+        try {
+            result = commandGateway.sendAndWait(command);
+        }catch (Exception e){
+            result = e.getLocalizedMessage();
+        }
+    }
 }
